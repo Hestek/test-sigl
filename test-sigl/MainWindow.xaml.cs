3d1cd5954b9403d29a7df6d7130a6050b373d376
Patrik Sigl <Sigl.Patrik@stud.vassboskovice.cz>
2022-05-17 11:26:38 +0200

    Přidejte soubory projektu.

---
+++ test-sigl/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test_sigl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        nakladak nakladak1;
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            nakladak1 = new nakladak();
+            Shownakladak(nakladak1, txt1);
+            
+
+        }
+
+
+        public void Shownakladak(nakladak vehicle, TextBox textbox)
+        {
+            textbox.Text = $"MaxNosnost: {vehicle.nosnost}\n";
+            textbox.Text += $"Naklad: {vehicle.naklad}\n";
+            textbox.Text += $"Velikost nadrze: {vehicle.velikost_nadrz}\n";
+            textbox.Text += $"Stav nadrze: {vehicle.stav_nadrz}\n";
+            textbox.Text += $"Tachometr: {vehicle.tachometr}\n";
+            textbox.Text += $"spotreba: {vehicle.spotreba}\n";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            nakladak1.Nalozitnaklad();
+            Shownakladak(nakladak1 , txt1);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            nakladak1.Vylozitnaklad();
+            Shownakladak(nakladak1, txt1);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            nakladak1.pohyb();
+            nakladak1.Spotreba();
+            Shownakladak(nakladak1, txt1);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            nakladak1.Natankovat();
+            Shownakladak(nakladak1, txt1);
+        }
+    }
+}
---
+++ test-sigl/nakladak.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_sigl
+{
+    public class nakladak
+    {
+        public int nosnost{ get; set; }
+        public int naklad { get; set; }
+        public int velikost_nadrz { get; set; }
+        public int stav_nadrz { get; set; }
+        public int tachometr { get; set; }
+        public int spotreba { get; set; }
+
+        public nakladak()
+        {
+            nosnost = 5000;
+            naklad = 0;
+            velikost_nadrz = 200;
+            stav_nadrz = 200;
+            tachometr = 0;
+            spotreba = 0;
+        }
+        public void Nalozitnaklad()
+        {
+        
+            if (naklad == 5000)
+            {
+                naklad = 5000;
+            }
+            else if(naklad < 5000)
+            {
+                naklad += 100;
+            }
+        }
+        public void Vylozitnaklad()
+        {
+            
+            if (naklad == 0)
+            {
+                naklad = 0;
+            }
+            else if(naklad > 0)
+            {
+                naklad -= 100;
+            }
+        }
+        public void pohyb()
+        {
+
+            if(stav_nadrz == 0)
+            {
+                tachometr += 0;
+                stav_nadrz = 0;
+
+            }
+            else if(naklad > 2500)
+            {
+                stav_nadrz -= 50;
+                tachometr += 50;
+            }
+            else if (naklad < 2500)
+            {
+                stav_nadrz -= 40;
+                tachometr += 40;
+            }
+        }
+        public void Spotreba()
+        {
+            if(stav_nadrz == 0)
+            {
+                spotreba = 0;
+            }
+            else if(naklad > 2500)
+            {
+                spotreba = 50;
+            }
+            else if(naklad < 2500)
+            {
+                spotreba = 40;
+            }
+        }
+        public void Natankovat()
+        {
+            stav_nadrz = 200;
+        }
+
+           
+            
+    }
+}
